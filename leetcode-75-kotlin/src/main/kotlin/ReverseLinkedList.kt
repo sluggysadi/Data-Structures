0511a831aeee96e1bed3b18550be87a9110c36cb
Sandeep Pandey <fromsandeeppandey@gmail.com>
2024-02-11 01:44:14 +0530

    Add a kotlin solution to reverse linked list

---
+++ leetcode-75-kotlin/src/main/kotlin/ReverseLinkedList.kt
@@ -0,0 +1,51 @@
+import commonclasses.ListNode
+
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2]
+ * Output: [2,1]
+ * Example 3:
+ *
+ * Input: head = []
+ * Output: []
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is the range [0, 5000].
+ * -5000 <= Node.val <= 5000
+ *
+ *
+ * Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+ * @see <a href="https://leetcode.com/problems/reverse-linked-list/">LeetCode</a>
+ *
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+fun reverseList(head: ListNode?): ListNode? {
+    var previousNode: ListNode? = null
+    var currentNode = head
+    while (currentNode != null) {
+        val nextNode = currentNode.next
+        currentNode.next = previousNode
+        previousNode = currentNode
+        currentNode = nextNode
+    }
+    return previousNode
+}
